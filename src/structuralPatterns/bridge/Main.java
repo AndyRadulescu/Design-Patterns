c226032cb95d5562b993a0e158ec7fb3b1fa6d49
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2019-05-29 14:06:11 +0200

    bridge pattern

---
+++ src/structuralPatterns/bridge/Main.java
@@ -0,0 +1,28 @@
+package structuralPatterns.bridge;
+
+import structuralPatterns.bridge.device.Device;
+import structuralPatterns.bridge.device.Radio;
+import structuralPatterns.bridge.device.Tv;
+import structuralPatterns.bridge.remote.AdvancedRemote;
+import structuralPatterns.bridge.remote.BasicRemote;
+
+public class Main {
+
+    private static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
---
+++ src/structuralPatterns/bridge/remote/Remote.java
@@ -0,0 +1,13 @@
+package structuralPatterns.bridge.remote;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
